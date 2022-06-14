6ef9295e9469fba40b1079c1e72c829e8007b531
N0N1m3 <n0n1m3.squad@gmail.com>
2022-06-14 20:29:55 +0300

    feat: Create track/similar endpoint

--- src/track/index.ts
+++ src/track/index.ts
@@ -2,6 +2,8 @@ import { YandexMusicClient } from "../client";
 
 import { log } from "../decorators/log.decorator";
 
+import { TrackSimilar } from "./similar.track";
+
 import { TrackSupplement } from "./supplement.track";
 
 export class Track {
@@ -15,5 +17,15 @@ export class Track {
 	@log()
 	public async supplement(id: string): Promise<TrackSupplement> {
 		return await this.client.request.get<TrackSupplement>(`/tracks/${id}/supplement`);
+	}
+
+  /**
+	 * Getting similar tracks.
+	 * @param {string} id The unique ID of the track.
+	 * @returns Similar tracks to the track..
+	 */
+	@log()
+	public async similar(id: string): Promise<TrackSimilar> {
+		return await this.client.request.get<TrackSimilar>(`/tracks/${id}/similar`);
 	}
 }
