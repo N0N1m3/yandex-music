d7e8f95397b2bf30bbecabb3e9f4bda4fb2c4ccd
N0N1m3 <n0n1m3.squad@gmail.com>
2022-06-14 21:17:34 +0300

    feat: Add track/get endpoint

--- src/track/index.ts
+++ src/track/index.ts
@@ -2,13 +2,17 @@ import { YandexMusicClient } from "../client";
 
 import { log } from "../decorators/log.decorator";
 
+import { list } from "../utils";
+
 import { TrackSimilar } from "./similar.track";
 
 import { TrackSupplement } from "./supplement.track";
 
+import { Track as TrackInterface } from "../interfaces";
+
 export class Track {
 	constructor(private readonly client: YandexMusicClient) {}
-  
+
 	/**
 	 * Getting more information about the track.
 	 * @param {string} id The unique ID of the track.
@@ -16,10 +20,12 @@ export class Track {
 	 */
 	@log()
 	public async supplement(id: string): Promise<TrackSupplement> {
-		return await this.client.request.get<TrackSupplement>(`/tracks/${id}/supplement`);
+		return await this.client.request.get<TrackSupplement>(
+			`/tracks/${id}/supplement`
+		);
 	}
 
-  /**
+	/**
 	 * Getting similar tracks.
 	 * @param {string} id The unique ID of the track.
 	 * @returns Similar tracks to the track..
@@ -27,5 +33,16 @@ export class Track {
 	@log()
 	public async similar(id: string): Promise<TrackSimilar> {
 		return await this.client.request.get<TrackSimilar>(`/tracks/${id}/similar`);
+	}
+
+	/**
+	 * Getting the track/tracks.
+	 * @param {number | Array<number>} ids The unique identifier of the track or tracks.
+	 * @returns Track or Tracks.
+	 */
+	@log()
+	public async get(ids: number | Array<number>) {
+		const [url, params] = list("track", ids);
+		return await this.client.request.get<TrackInterface>(url, params);
 	}
 }
