172376b3a5b8a8f2daaba8a8436213e96a1d190a
N0N1m3 <n0n1m3.squad@gmail.com>
2022-06-15 18:36:56 +0300

    feat: Create users/playlist/recommendations endpoint

--- src/users/playlist/index.ts
+++ src/users/playlist/index.ts
@@ -4,6 +4,8 @@ import { log } from "../../decorators/log.decorator";
 
 import { PlaylistInterface } from "./get.playlist";
 
+import { PlaylistsRecommendations } from "./recommendations.playlist";
+
 export class Playlists {
 	constructor(private readonly client: YandexMusicClient) {}
 
@@ -14,9 +16,20 @@ export class Playlists {
    * @returns Playlist List.
    */
 	@log()
-	public async get(id: string, kind: number | Array<number>) {
+	public async get(id: string, kind: number | Array<number>): Promise<Array<PlaylistInterface>> {
 		const params = { kinds: kind.toString() };
 		const playlists = await this.client.request.get<Array<PlaylistInterface>>(`/users/${id}/playlists`, params);
 		return playlists.filter((playlist) => playlist?.owner !== undefined);
+	}
+  
+  /**
+   * Getting a playlist or playlist list by unique identifiers.
+   * @param {string} id The unique ID of the user who owns the playlist.
+   * @param {number } kind The unique ID of the playlist.
+   * @returns Playlist List.
+   */
+	@log()
+	public async recommendations(id: string, kind: number): Promise<Array<PlaylistsRecommendations>> {
+		return await this.client.request.get<Array<PlaylistsRecommendations>>(`/users/${id}/playlists/${kind}/recommendations`);
 	}
 }
