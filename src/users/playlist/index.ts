219c025dc17237103720f297afa49ee5b62cedd6
N0N1m3 <n0n1m3.squad@gmail.com>
2022-06-15 19:31:49 +0300

    feat: Create users/playlists/create|delete endpoints

--- src/users/playlist/index.ts
+++ src/users/playlist/index.ts
@@ -25,11 +25,35 @@ export class Playlists {
   /**
    * Getting a playlist or playlist list by unique identifiers.
    * @param {string} id The unique ID of the user who owns the playlist.
-   * @param {number } kind The unique ID of the playlist.
+   * @param {number} kind The unique ID of the playlist.
    * @returns Playlist List.
    */
 	@log()
 	public async recommendations(id: string, kind: number): Promise<Array<PlaylistsRecommendations>> {
 		return await this.client.request.get<Array<PlaylistsRecommendations>>(`/users/${id}/playlists/${kind}/recommendations`);
+	}
+
+  /**
+   * Creating a playlist.
+   * @param {string} id The unique ID of the owner.
+   * @param {string} title The title of playlist.
+   * @param {string} visibility Access modifier. 
+   * @returns New Playlist.
+   */
+	@log()
+	public async create(id: string, title: string, visibility: "public" | "private" = "public"): Promise<PlaylistInterface> {
+		const params = { title, visibility };
+		return await this.client.request.post<PlaylistInterface>(`/users/${id}/playlists/create`, null, params);
+	}
+
+  /**
+   * Deleting a playlist
+   * @param {string} id The unique ID of the owner.
+   * @param {string} kind The unique ID of the playlist.
+   * @returns OK in success
+   */
+	@log()
+	public async delete(id: string, kind: string): Promise<string> {
+		return await this.client.request.post<string>(`/users/${id}/playlists/${kind}/delete`);
 	}
 }
