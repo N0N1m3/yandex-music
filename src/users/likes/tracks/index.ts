b62c1785dce78f19f8413b953de6794c8b48f6cd
N0N1m3 <n0n1m3.squad@gmail.com>
2022-06-16 18:55:19 +0300

    feat: Create users/likes/tracks/get endpoint

--- src/users/likes/tracks/index.ts
+++ src/users/likes/tracks/index.ts
@@ -2,6 +2,17 @@ import { Likes } from "..";
 
 import { log } from "../../../decorators/log.decorator";
 
+import { Track } from "../../../interfaces";
+
+interface TracksGet {
+	library: {
+		uid: number,
+		revision: number,
+		playlistUuid: string,
+		tracks: Array<Track>
+	}
+}
+
 export class Tracks {
 	constructor(private readonly likes: Likes) {}
 
@@ -17,13 +28,23 @@ export class Tracks {
 	}
 
 	/**
-	* Remove the "I like" mark from the track/tracks.
-	* @param {number | Array<number>} ids The unique identifier of the track or tracks.
-	* @param {string} user_id Unique user ID.
-	* @returns OK if the request is successful.
-	*/
+	 * Remove the "I like" mark from the track/tracks.
+	 * @param {number | Array<number>} ids The unique identifier of the track or tracks.
+	 * @param {string} user_id Unique user ID.
+	 * @returns OK if the request is successful.
+	 */
 	@log()
 	public async remove(ids: number | Array<number>, user_id: number = this.likes.client.uid): Promise<string> {
 		return await this.likes.action("track", ids, true, user_id);
+	}
+
+	/**
+	 * Getting tracks marked "I like".
+	 * @param {number} user_id Unique user ID.
+	 * @returns Tracks info.
+	 */
+	@log()
+	public async get(user_id: number = this.likes.client.uid) {
+		return await this.likes.get<Array<TracksGet>>("track", user_id)
 	}
 }
