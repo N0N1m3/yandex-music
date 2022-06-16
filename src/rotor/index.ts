de7948981117c22b87b7ab1956b6b568f1cac521
N0N1m3 <n0n1m3.squad@gmail.com>
2022-06-16 16:55:31 +0300

    feat: Create rotor/settings endpoint

--- src/rotor/index.ts
+++ src/rotor/index.ts
@@ -14,7 +14,15 @@ import { RotorStatusInterface } from "./status.rotor";
 
 import { RotorDashboardInterface } from "./dashboard.rotor";
 
-type FeedbackType = "radioStarted" | "trackStarted" | "trackFinished" | "skip"
+type FeedbackType = "radioStarted" | "trackStarted" | "trackFinished" | "skip";
+
+type energy = "fun" | "active" | "calm" | "sad" | "all";
+
+type diver = "favorite" | "popular" | "discover" | "default";
+
+type lang = "not-russian" | "russian" | "any";
+
+type settings = "rotor" | "generative"
 
 export class Rotor {
 	public readonly radio: Radio;
@@ -70,5 +78,20 @@ export class Rotor {
 	@log()
 	public async info(station: string): Promise<RotorListInterface> {
 		return await this.client.request.get<RotorListInterface>(`/rotor/station/${station}/info`);
+	}
+
+	/**
+	 * Changing the settings of a specific station.
+	 * @param {string} station Station.
+	 * @param {energy} moodEnergy Mood.
+	 * @param {diver} diversity The type of tracks.
+	 * @param {lang} language Language.
+	 * @param {settings} type Type.
+	 * @returns OK if the request is successful.
+	 */
+	@log()
+	public async settings(station: string, moodEnergy: energy, diversity: diver, language: lang = "any", type: settings = "rotor"): Promise<string> {
+		const data = { moodEnergy, diversity, type, language }
+		return await this.client.request.post<string>(`/rotor/station/${station}/settings3`, data);
 	}
 }
