fbe91d9e109d3e8630f973b23d9b417424fc33d3
N0N1m3 <n0n1m3.squad@gmail.com>
2022-06-16 17:50:19 +0300

    feat: Create users/likes/action endpoint

---
+++ src/users/likes/index.ts
@@ -0,0 +1,38 @@
+import { YandexMusicClient } from "../../client";
+
+import { log } from "../../decorators/log.decorator";
+
+import { Albums } from "./albums";
+import { Artists } from "./artists";
+import { Playlists } from "./playlists";
+import { Tracks } from "./tracks";
+
+type action = "track" | "artist" | "playlist" | "album";
+
+export class Likes {
+	public readonly albums: Albums;
+	public readonly artists: Artists;
+	public readonly playlists: Playlists;
+	public readonly tracks: Tracks;
+	constructor(public readonly client: YandexMusicClient) {
+		this.albums = new Albums(this);
+		this.artists = new Artists(this);
+		this.playlists = new Playlists(this);
+		this.tracks = new Tracks(this);
+	}
+
+  /**
+   * Actions marked "I like".
+   * @param {action} type The type of the object.
+   * @param {number | Array<number>} ids 
+   * @param {boolean} remove If True, then removes the mark, otherwise puts.
+   * @param {number} user_id Unique user ID.
+   * @returns OK if the request is successful.
+   */
+	@log()
+	public async action(type: action, ids: number | Array<number>, remove: boolean = false, user_id: number = this.client.uid): Promise<string> {
+		const action = remove ? "remove" : "add-multiple";
+		const params = { [`${type}-ids`]: ids };
+		return await this.client.request.post<string>(`/users/${user_id}/likes/${type}s/${action}`, null, params);
+	}
+}
