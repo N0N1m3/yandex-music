103914470a17c7f42796b32468d14249b2213819
N0N1m3 <n0n1m3.squad@gmail.com>
2022-06-16 18:41:59 +0300

    feat: Create users/likes/artists/get|with endpoints

--- src/users/likes/artists/index.ts
+++ src/users/likes/artists/index.ts
@@ -2,6 +2,13 @@ import { Likes } from "..";
 
 import { log } from "../../../decorators/log.decorator";
 
+import { Artist } from "../../../interfaces";
+
+interface ArtistGet {
+	artist: Artist;
+	timestamp: string;
+}
+
 export class Artists {
 	constructor(private readonly likes: Likes) {}
 
@@ -25,5 +32,25 @@ export class Artists {
 	@log()
 	public async remove(ids: number | Array<number>, user_id: number = this.likes.client.uid): Promise<string> {
 		return await this.likes.action("artist", ids, true, user_id);
+	}
+
+	/**
+	 * Getting artists marked "I like".
+	 * @param {number} user_id Unique user ID.
+	 * @returns Short artists info.
+	 */
+	@log()
+	public async get(user_id: number = this.likes.client.uid) {
+		return await this.likes.get<Array<Artist>>("artist", user_id)
+	}
+
+	/**
+	 * Getting artists marked "I like".
+	 * @param {number} user_id Unique user ID.
+	 * @returns Rich artists info.
+	 */
+	@log()
+	public async with(user_id: number = this.likes.client.uid) {
+		return await this.likes.get<Array<ArtistGet>>("artist", user_id, {'rich': "false"})
 	}
 }
