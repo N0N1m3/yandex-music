8ada4248c30cf185fe528a2cea5876ef033d5bf7
N0N1m3 <n0n1m3.squad@gmail.com>
2022-06-16 19:05:36 +0300

    feat: Add users/likes endpoints

--- src/users/index.ts
+++ src/users/index.ts
@@ -2,12 +2,16 @@ import { YandexMusicClient } from "../client";
 
 import { log } from "../decorators/log.decorator";
 
+import { Likes } from "./likes";
+
 import { Playlists } from "./playlist";
 
 export class Users {
 	public readonly playlists: Playlists;
+	public readonly likes: Likes
 
 	constructor(private readonly client: YandexMusicClient) {
 		this.playlists = new Playlists(client);
+		this.likes = new Likes(client)
 	}
 }
