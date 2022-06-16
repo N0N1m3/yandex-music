fbeb68bc3bfc3541fc6ed2c4f27403eaa5689346
N0N1m3 <n0n1m3.squad@gmail.com>
2022-06-16 18:32:37 +0300

    feat: Create users/likes/albums/get|rich endpoints

--- src/users/likes/albums/index.ts
+++ src/users/likes/albums/index.ts
@@ -2,6 +2,18 @@ import { Likes } from "..";
 
 import { log } from "../../../decorators/log.decorator";
 
+import { Album } from "../../../interfaces";
+
+interface AlbumsGet {
+	id: number;
+	timestamp: string;
+}
+
+interface AlbumsRich {
+	album: Album;
+	timestamp: string;
+}
+
 export class Albums {
 	constructor(private readonly likes: Likes) {}
 
@@ -25,5 +37,25 @@ export class Albums {
 	@log()
 	public async remove(ids: number | Array<number>, user_id: number = this.likes.client.uid): Promise<string> {
 		return await this.likes.action("album", ids, true, user_id);
+	}
+
+	/**
+	 * Getting albums marked "I like".
+	 * @param {number} user_id Unique user ID.
+	 * @returns Short albums info.
+	 */
+	@log()
+	public async get(user_id: number = this.likes.client.uid) {
+		return await this.likes.get<Array<AlbumsGet>>("album", user_id, {'rich': "false"})
+	}
+	
+	/**
+	 * Getting albums marked "I like".
+	 * @param {number} user_id Unique user ID.
+	 * @returns Rich albums info.
+	 */
+	@log()
+	public async rich(user_id: number = this.likes.client.uid) {
+		return await this.likes.get<Array<AlbumsRich>>("album", user_id, {'rich': "true"})
 	}
 }
