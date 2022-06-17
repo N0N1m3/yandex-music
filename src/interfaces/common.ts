3d553d1da98bf3c284e938360e5ae9e66b4207ef
N0N1m3 <n0n1m3.squad@gmail.com>
2022-06-17 23:07:22 +0300

    feat: Create common interfaces

---
+++ src/interfaces/common.ts
@@ -0,0 +1,29 @@
+export type Union = string | number;
+export type List = Array<Union>;
+
+export type Lad = { id: number; name: string };
+
+export type CoverBase = { type: string; uri: string };
+
+export type Cover = CoverBase & { prefix: string };
+
+export type CoverPlaylist = CoverBase & {
+	dir: string;
+	version: string;
+	custom: boolean;
+};
+
+export interface SocialLink {
+	title: string;
+	href: string;
+	type: string;
+	socialNetwork: string;
+}
+
+export interface Owner {
+	uid: number;
+	login: string;
+	name: string;
+	sex: string;
+	verified: boolean;
+}
